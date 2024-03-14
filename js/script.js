bb6a8e1dc649d46904cfeafa9f4352c0a37b41eb
Benedettopana <benedetto.pana@gmail.com>
2024-03-14 14:22:41 +0100

    [add]Milestone-1

--- js/script.js
+++ js/script.js
@@ -6,9 +6,20 @@ createApp({
   data(){
     return{
       contacts,
-
+      last: '',
       
     }
+  },
+
+  methods:{
+    // lastmsg
+    lastMsg(){
+      // last = `${this.contacts.messages[messages.legth-1].message}`;
+    },
+  },
+
+  mounted(){
+    this.lastMsg();
   },
 
 }).mount('#app');
