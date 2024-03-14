8e59dc4963cc5f5d7ffb38fb485508b39c7b3237
Benedettopana <benedetto.pana@gmail.com>
2024-03-14 17:33:51 +0100

    [add]Milestone-3

--- js/script.js
+++ js/script.js
@@ -6,28 +6,62 @@ createApp({
   data(){
     return{
       contacts,
-      last: '',
-
-      // history
-      msgHistory: [],
-      // Obj msg
-      msgObj: {
+      lastTime: null,
+      lastMsg: null,
+      activePerson: 0,
+      newMsg: '',
+      newObj:{
         date: '',
         message: '',
-        status: 'sent'
+        status: ''
       },
-
-      activePerson: 0,
     }
   },
 
   methods:{
+    last(){
+      const temp = this.contacts[this.activePerson].messages;
+      console.log(temp,temp.length);
+      return this.lastTime = temp[temp.length-1].date, this.lastMsg = temp[temp.length-1].message;
+    },
+    
+    addMsg(){
+      
+      this.newObj = {
+        date: '13/03/2024 16:09:55',
+        message: this.newMsg,
+        status: 'sent'
+      }
+      
+      this.contacts[this.activePerson].messages.push(this.newObj);
+      
+      // RESET
+      this.newObj = {
+        date: '',
+        message: '',
+        status: ''
+      }
+      this.newMsg = '';
+      this.autoReply();
+    },
+
+    autoReply(){
+      setTimeout(() => {
+        this.newObj = {
+          date: '10/03/2024',
+          message: 'Ok',
+          status: 'received'
+        };
+        this.contacts[this.activePerson].messages.push(this.newObj);
+        
+      }, 1000)
+    }
 
 
   },
 
   mounted(){
-  
+    this.last();
   },
 
 }).mount('#app');
